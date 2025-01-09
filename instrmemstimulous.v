e861dabfab9601461af8ef27a61443f1b54b1ea5
Aman Kumar Singh <102130326+AmanKumarSingh619@users.noreply.github.com>
2025-01-09 22:57:24 +0530

    Update instrmemstimulous.v

--- instrmemstimulous.v
+++ instrmemstimulous.v
@@ -1,26 +1,24 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.01.2025 22:26:29
-// Design Name: 
-// Module Name: instrmemstimulous
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module instrmemstimulous();
 
+reg [31:0] addr;
+wire [31:0] instr;
 
-module instrmemstimulous(
+InstructionMem instructionmemory(instr, addr);
 
-    );
+initial
+begin
+$monitor("Mem Address=%h instruction=%b",addr,instr);
+addr=32'd0;
+#10000 addr=32'd4;
+#10000 addr=32'd8;
+#10000 addr=32'd12;
+#10000 addr=32'd16;
+#10000 addr=32'd20;
+#10000 addr=32'd24;
+#10000 addr=32'd28;
+#10000;
+$finish;
+end
+
 endmodule
